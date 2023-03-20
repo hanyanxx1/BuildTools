edd414c5c7913e7a9b589a6e61e9caa4f4ec1e1a
hanyanxx1 <hanyanxx1@163.com>
2023-03-20 10:14:31 +0800

    深入Webpack5等构建工具(gulprollupvite)

---
+++ 深入Webpack5等构建工具(gulprollupvite)/02_learn_webpack2/23_webpack中代码压缩/build/js/main.7dbb92.bundle.js
@@ -0,0 +1,5 @@
+(self.webpackChunkwebpack_devserver=self.webpackChunkwebpack_devserver||[]).push([["main"],{"./src/main.js":
+/*!*********************************!*\
+  !*** ./src/main.js + 5 modules ***!
+  \*********************************/(e,n,s)=>{"use strict";s.e(/*! import() */"src_abc_js").then(s.bind(s,/*! ./abc */"./src/abc.js")).then((function(e){})),console.log(function sum(e,n){return e+n}(20,30)),console.log(function math_sum(e,n){return e+n}(20,30)),console.log("2021-11-11"),console.log(window.abc)}},0,[["./src/main.js","runtime"]]]);
+//# sourceMappingURL=main.7dbb92.bundle.js.map
